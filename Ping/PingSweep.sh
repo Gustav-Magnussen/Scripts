6431299095f9025b6438ce2b3b39100da463b596
Gustav-Magnussen <59768512+Gustav-Magnussen@users.noreply.github.com>
2020-08-30 14:54:22 +0200

    Update PingSweep.sh

--- Ping/PingSweep.sh
+++ Ping/PingSweep.sh
@@ -3,6 +3,19 @@
 IP=$(ip address | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sed -n '2 p')
 SUB=$(echo $IP | rev | cut -d. -f2- | rev)
 
-for numb in {1..254}; do :
-    echo $(ping -c $SUB.$numb) 
-done
+MASK=$(ip -o -f inet addr show | awk '/scope global/ {print $4}' | cut -d/ -f2)
+
+if [[ $MASK == "24" ]]; then
+    for numb in {1..254}; do :
+        echo $(ping -c 1 $SUB.$numb) 
+    done
+    
+elif [[ $MASK == "16" ]]; then
+    SUB2=$(echo $SUB | rev | cut -d. -f2- | rev)
+    for numb1 in {1..254}; do
+        for numb2 in {1..254}; do 
+        echo $(ping -c 1 $SUB2.$numb1.$numb2) 
+        done
+        
+    done
+fi
